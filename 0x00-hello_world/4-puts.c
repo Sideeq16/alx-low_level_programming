6c7b2b9737cb6b91ae0a70154292cb309117b5b7
Sideeq16 <ajikuru3@gmail.com>
2023-03-16 21:46:53 -0700

    tast 2-3-4-5-6 complteted

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry  point
+ * puts: programing is like building a multigual puzzle
+ * Return: success (0)
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry  point
+ * printf: programing is like building a multigual puzzle
+ * Return: success (0)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
