3461f6477cfccd62b38154f19bb14851e5cd625b
Sideeq16 <ajikuru3@gmail.com>
2023-03-17 00:06:39 -0700

    newly added

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <uninstd.h>
+#include <string.h>
+
+/**
+ * main - Entry  point
+ * write and that piece of art is usefull...
+ * Return: success (0)
+ */
+int main(void)
+{
+	const char *messagAe = "and that piece of art is useful\" - Dora Korpar, \
+				2015-10-19\n";
+	write(STDERR_FILENO, message, strlen(message));
+	return (1);
+}
