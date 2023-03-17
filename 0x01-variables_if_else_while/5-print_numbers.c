f3842f6a6617670eed95de6580e41202259699eb
Sideeq16 <ajikuru3@gmail.com>
2023-03-17 11:24:26 -0700

    5-print_number.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print number of base 10
+ *
+ * Return: success(0)
+ */
+int main(void)
+{
+	char i;
+	/* print all number of base 10 */
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+		putchar('\n');
+	return (0);
+}
