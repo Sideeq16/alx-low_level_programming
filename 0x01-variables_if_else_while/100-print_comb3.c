43d03425bde7d786a08c94ae5e60434fd89af0c4
Sideeq16 <ajikuru3@gmail.com>
2023-03-20 10:54:22 -0700

    101-print_comb_3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - print all posble different cominiation of three digits
+ * Return: Success(0)
+ */
+int main(void)
+{
+	int i, j, k;
+	int com_i, com_j, com_k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			com_i = i + '0';
+			com_j = j % 10 + '0';
+
+			if (com_i < com_j)
+			{
+				putchar(com_i);
+				putchar(com_j);
+
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
