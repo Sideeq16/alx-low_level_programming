c120bb3bafd3ec6ed5b307d559f6f3bfed6dd4fc
Sideeq16 <ajikuru3@gmail.com>
2023-03-21 07:05:16 -0700

    Task-1: 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print out text with define function
+ * Return: _putchar
+ */
+int main(void)
+{
+	
+	_putchar('\n');
+
+	return (0);
+}
+
+void print_alphabet()
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+		_putchar('\n');
+}
