16fb439b9c5ade722acd35a289844206bf375091
Sideeq16 <ajikuru3@gmail.com>
2023-03-23 22:14:52 +0000

    fix fuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,16 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+* main - print digit and fizz buzz
+* Return: successs (0)
+*/
+int main(void)
+{
+	fizz_buzz();
+
+	return (0);
+}
 
 /**
 * fizz_buzz - Fizz Buzz test
@@ -12,27 +24,25 @@ void fizz_buzz(void)
 	{
 		if ((i % 3) == 0 && (i % 5) ==  0)
 		{
-			/* printf("%s", "FizzBuzz"); */
+			printf("%s", "FizzBuzz");
 		}
 		else if ((i % 3) == 0)
 		{
-			/* printf("%s", "Fizz"); */
+			printf("%s", "Fizz");
 		}
 		else if ((i % 5) == 0)
 		{
-			/* printf("%s", "Buzz"); */
+			printf("%s", "Buzz");
 		}
 		else
 		{
-			/* printf("%d", i); */
+			printf("%d", i);
 		}
 
 		if (i != 100)
 		{
-			/* printf(" "); */
+			printf(" ");
 		}
 	}
 	printf("\n");
-
-	return (0);
 }
