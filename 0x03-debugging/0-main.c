84e363465fd83c169780ac8b5e93f346368dfda1
Sideeq16 <ajikuru3@gmail.com>
2023-03-23 10:59:28 +0000

    debugging negative_positve

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test function positive_or_negative
+ * Return: (0) for success
+ */
+int main(void)
+{
+	int r;
+
+	r = positive_or_negative(0);
+	printf(r);
+
+	return (0);
+}
