c6543a86972fdabe511ddd3e9147f9359bf95366
Sideeq16 <ajikuru3@gmail.com>
2023-03-27 18:54:07 +0000

    A good engineer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+* rev_string - print string in reverse using it pointer address
+* @s: take string to be reverse
+*/
+void rev_string(char *s)
+{
+	int m_size = 0;
+
+	while (s[count] != '\0')
+	{
+		m_size++;
+	}
+	int i = m_size - 1;
+	char clone_s[i];
+	int strSize = i;
+	int j = 0;
+	int k = 0;
+
+	while (i >= 0)
+	{
+		clone_s[j] = s[i];
+		j++;
+		i--;
+	}
+
+	while (k <= strSize)
+	{
+		*(s + k) = clone_s[k];
+		k++;
+	}
+}
+
