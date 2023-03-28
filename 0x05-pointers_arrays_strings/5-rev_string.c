cb810c810be51ef9197cdf5ce12994d591da03f1
Sideeq16 <ajikuru3@gmail.com>
2023-03-28 01:34:41 +0000

    fix reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,30 +6,19 @@
 */
 void rev_string(char *s)
 {
-	int m_size = 0;
-	int i;
-	char *clone_s = s;
-	int strSize, j = 0, k = 0;
+	int i = 0, mysize = 0;
+	char temp;
 
 
-	while (s[m_size] != '\0')
+	while (s[mysize] != '\0')
 	{
-		m_size++;
+		mysize++;
 	}
-	i = (m_size - 1);
-	strSize = i;
-
-	while (i >= 0)
+	for (i = 0; i < mysize / 2; i++)
 	{
-		clone_s[j] = s[i];
-		j++;
-		i--;
-	}
-
-	while (k <= strSize)
-	{
-		*(s + k) = clone_s[k];
-		k++;
+		temp = s[i];
+		s[i] = s[mysize - i - 1];
+		s[mysize - i - 1] = temp;
 	}
 }
 
