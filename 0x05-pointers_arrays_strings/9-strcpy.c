39adb64b5dae48a7e96cb6d80e181fb8ecee4e0f
Sideeq16 <ajikuru3@gmail.com>
2023-03-28 03:51:53 +0000

    fix strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* *_strcpy - print arrays
+* @dest: destination to copy to;
+* @src: characters to copy;
+* Return: source string upon successful
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0, mysize = 0;
+
+	while (src[mysize] != '\0')
+	{
+		mysize++;
+	}
+
+	for (i = 0; i < mysize; i++)
+	{
+		*(dest + i) = src[i];
+	}
+	_putchar('\n');
+	return (src);
+}
+
