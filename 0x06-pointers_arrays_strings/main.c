faade4efef6cc27f4e28be2d5081c32022ec887d
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-03-31 19:09:40 +0100

    modify main.c

--- 0x06-pointers_arrays_strings/main.c
+++ 0x06-pointers_arrays_strings/main.c
@@ -8,11 +8,15 @@
 */
 int main(void)
 {	
-	char s1[] = "Hello";
-	char s2[] = "World!";
-
-	printf("%d\n", _strcmp(s1, s2));
-	printf("%d\n", _strcmp(s2, s1));
-	printf("%d\n", _strcmp(s1, s1));
-	return (0);
+    print_number(98);
+    _putchar('\n');
+    print_number(402);
+    _putchar('\n');
+    print_number(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_number(-98);
+    _putchar('\n');
+    return (0);
 }
