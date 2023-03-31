8b42e2d4f408ea8bfaf113160bb85a86b1a48538
Sideeq16 <ajikuru3@gmail.com>
2023-03-31 11:03:55 +0000

    lett 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,22 +9,25 @@ char *leet(char *str)
 {
 	int i = 0, j;
 
-    const char *p = "a4A4e3E3o0O0t7T7l1L1";
-	
-	while(str[i] != '\0'){
-        char c = str[i];
+	const char *p = "a4A4e3E3o0O0t7T7l1L1";
+
+	while (str[i] != '\0')
+	{
+
+	char c = str[i];
+
 	j = 0;
-	while(p[j] != '\0'){
-            if (c == p[j]) {
-                str[i] = p[j+1];
-                break;
-            }
-		j++;
+
+		while (p[j] != '\0')
+		{
+			if (c == p[j])
+			{
+				str[i] = p[j + 1];
+				break;
+			}
+			j++;
+		}
+	i++;
 	}
-	    i++;
-    }
-
-
-
 	return (str);
 }
