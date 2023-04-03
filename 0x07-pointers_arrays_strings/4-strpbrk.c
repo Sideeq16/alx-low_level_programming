4cb9ab14f5b2ddc1f94d50305ad9313f55e871d8
Sideeq16 <ajikuru3@gmail.com>
2023-04-03 20:36:23 +0000

    added 4-strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,18 +1,35 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * *_strpbrk - filling available memory space with a constant value
+ * @s: buffer to use
+ * @accept: constant for filing
+ * Return: buffter of the array
  */
-int main(void)
+char *_strpbrk(char *s, char *accept)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
+	int i = 0;
+	int j = 0;
+	int signal = 0;
 
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
+	while (s[i] != '\0')
+	{
+		j = 0;
+		signal = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				signal = 1;
+				break;
+			}
+			j++;
+		}
+		if (signal == 1)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (0);
 }
