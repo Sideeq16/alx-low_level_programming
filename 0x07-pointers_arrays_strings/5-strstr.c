31e1f4533fee2fb8feaa3b5055e4146a0caa947a
Sideeq16 <ajikuru3@gmail.com>
2023-04-04 11:22:41 +0000

    added strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,18 +1,40 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * *_strstr - find if string exist
+ * @s: string to use
+ * @accept: stringto search
+ * Return: buffter of the array
  */
-int main(void)
+char *_strstr(char *s, char *accept)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
+	int i = 0;
+	int j = 0;
+	int signal = 1;
+	int k = 0;
 
-    t = _strstr(s, f);
-    printf("%s\n", t);
-    return (0);
+	while (s[i] != '\0')
+	{
+		k = i;
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[k] == accept[j])
+			{
+				signal = 0;
+			}
+			else
+			{
+				signal = 1;
+			}
+			k++;
+			j++;
+		}
+		if (signal == 0)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (0);
 }
