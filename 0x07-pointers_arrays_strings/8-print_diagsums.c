d167d3e91123bd28b1f62563d90a42098d7d211f
Sideeq16 <ajikuru3@gmail.com>
2023-04-04 13:18:48 +0000

    Added print_diagsum

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,25 +2,23 @@
 #include <stdio.h>
 
 /**
- * main - check the code
+ * print-diagsums - sum diagonal of a square matrix
  *
  * Return: Always 0.
  */
-int main(void)
+void print_diagsums(int *a, int size)
 {
-    int c3[3][3] = {
-        {0, 1, 5},
-        {10, 11, 12},
-        {1000, 101, 102},
-    };
-    int c5[5][5] = {
-        {0, 1, 5, 12124, 1234},
-        {10, 11, 12, 123521, 12512},
-        {1000, 101, 102, 12545, 214543435},
-        {100, 1012451, 11102, 12545, 214543435},
-        {10, 12401, 10452, 11542545, 1214543435},
-    };
-    print_diagsums((int *)c3, 3);
-    print_diagsums((int *)c5, 5);
-    return (0);
+	int j = 0;
+	int i = 0;
+	int diagA = 0;
+	int diagB = 0;
+	j = size - 1;
+	
+	for (i = 0; i < size; i++)
+	{
+		diagA += *(a + i + j * size);
+		diagB += *(a + i + i * size);
+		j--; 
+	}
+	printf("%d, %d\n", diagA, diagB);
 }
