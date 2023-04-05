d8d68caac29a882295397249c333bf19edd2a0bd
Sideeq16 <ajikuru3@gmail.com>
2023-04-05 16:56:44 +0000

    added recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string reverse
+ * @s: string to reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	int len = 0;
+	
+	if (*s == '\n')
+	{
+		while (s[len] != '\0')
+		{
+			len++;
+		}
+		_print_rev_recursion(s + (len));
+	}
+
+	if (*s != '\n')
+	{
+		if (*(s - 1) != '\n')
+		{
+			_putchar(*(s - 1));
+			_print_rev_recursion(s - 1);
+		}
+		else
+		{
+			_putchar('\n');
+		}
+	}
+}
