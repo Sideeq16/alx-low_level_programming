649d73391076b4d9714211ccba71facc7d94aca6
Sideeq16 <ajikuru3@gmail.com>
2023-04-06 14:36:03 +0000

    Fixed palind

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,28 +1,23 @@
 #include "main.h"
-#include <stdio.h>
 #include <string.h>
 
 /**
  * is_palindrome_helper - helper class for palindrome
  * @word: string to use
  * @str: string of test to search frome
- * @store: keep tracking of string
  * Return: zero or 1
  */
-int is_palindrome_helper(char *word, char *str, char *store)
+int is_palindrome_helper(char *word, char *str)
 {
 	int len = strlen(str) - 1;
-	int len_store = strlen(store);
 
-	store[len_store] = word[len];
-
-	if (store[len_store] != str[0])
+	if (word[len] != str[0])
 	{
 		return (0);
 	}
 	if (len != 0)
 	{
-		is_palindrome_helper(word, str + 1, store);
+		is_palindrome_helper(word, str + 1);
 	}
 	else
 	{
@@ -41,9 +36,8 @@ int is_palindrome(char *s)
 {
 
 	int len = strlen(s);
-	char *save[len];
 
 	if (*s == '\0')
 		return (1);
-	return (is_palindrome_helper(s, s, save));
+	return (is_palindrome_helper(s, s));
 }
