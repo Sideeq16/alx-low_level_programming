705af29daa12967df2758d1e41b771fdf606a60e
Sideeq16 <ajikuru3@gmail.com>
2023-04-08 11:12:21 +0000

    fix error

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,14 +9,18 @@
  */
 int main(int argc, char *argv[])
 {
+	int total = 0;
+
 	if (argc < 3 || argc > 3)
 	{
 		printf("%s\n", "Error");
+		return (1);
 	}
 	else
 	{
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		total = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", total);
 	}
 
-	return (1);
+	return (0);
 }
