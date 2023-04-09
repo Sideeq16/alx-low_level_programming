353a3895d180fa0c6bc5310c1371f1f79ea2c86c
Sideeq16 <ajikuru3@gmail.com>
2023-04-09 11:59:21 +0000

    fixed b'

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-find . -name '*.c' -exec gcc -c {} \; && find . -name '*.o' -exec ar rc liball.a {} \; && ranlib liball.a; && echo -e;
+find . -name '*.c' -exec gcc -c {} \; && find . -name '*.o' -exec ar rc liball.a {} \; && ranlib liball.a;
