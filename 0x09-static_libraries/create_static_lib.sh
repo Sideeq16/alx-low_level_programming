d60bae9cc5f0653193eacc518600188c3e9911ab
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-04-09 16:46:12 +0100

    Update

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,10 @@
 #!/bin/bash 
 
 # Compile all .c files in the current directory into object files 
-
 gcc -c *.c 
 
 # Archive the object files into a static library 
-
 ar rcs liball.a *.o 
 
-rm *.o 
+# Clean up the object files
+rm *.o
