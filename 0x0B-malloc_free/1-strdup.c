b90acd02fca21a57557359e44a730788241c3cd3
Sideeq16 <ajikuru3@gmail.com>
2023-04-11 10:13:39 +0000

    fix strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,14 +9,16 @@
 */
 char *_strdup(char *str)
 {
+	char *ptr;
+	unsigned int i = 0;
+	unsigned int size;
+	
 	if (str == NULL)
 	{
 		return (NULL);
 	}
 
-	char *ptr;
-	unsigned int i = 0;
-	unsigned int size = strlen(str);
+	size = strlen(str);
 
 	ptr = (char *) malloc(size * sizeof(char));
 
