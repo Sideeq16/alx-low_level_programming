35742890f1289cad5ea3ea179e6fdbfe5a26d004
Sideeq16 <ajikuru3@gmail.com>
2023-04-11 14:06:28 +0000

    fixed allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,6 +20,12 @@ int **alloc_grid(int width, int height)
 
 	ptr = (int **) malloc(height * sizeof(int *));
 
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+
 	for (i = 0; i < height; i++)
 		ptr[i] = (int *) malloc(width * sizeof(int));
 
