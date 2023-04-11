d756e08f67ee10a54708fed9968cd10d31a666ae
Sideeq16 <ajikuru3@gmail.com>
2023-04-11 18:02:35 +0000

    3 allocate

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,7 +29,14 @@ int **alloc_grid(int width, int height)
 
 
 	for (i = 0; i < height; i++)
+	{
 		ptr[i] = (int *) malloc(width * sizeof(int));
+		if (ptr[i] == NULL)
+		{
+			free(ptr[i]);
+		}
+	}
+
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
