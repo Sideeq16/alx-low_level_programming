4a9986f4dd464235a21feaa284cbdcf4992e8a46
Sideeq16 <ajikuru3@gmail.com>
2023-04-11 17:47:30 +0000

    last task

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -37,16 +37,19 @@ void print_grid(int **grid, int width, int height)
 int main(void)
 {
     int **grid;
-
-    grid = alloc_grid(6, 4);
+	int h;
+	int w;
+	h = 6;
+	w = 4;
+    grid = alloc_grid(h, w);
     if (grid == NULL)
     {
         return (1);
     }
-    print_grid(grid, 6, 4);
+    print_grid(grid, h, w);
     printf("\n");
     grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
+    grid[3][4] = 402; 
+   print_grid(grid, h, w);
     return (0);
 }
---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_grid - free allocated space
+ * @grid: grid allocated
+ * @height: heigth of grid
+ * Return: zero on successful;
+*/
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	if (grid != NULL)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+	}
+
+}
