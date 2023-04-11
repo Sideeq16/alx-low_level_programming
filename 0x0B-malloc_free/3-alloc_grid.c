896268a80ab2c2b7a8dd61a1d61fa599fc6cb716
Sideeq16 <ajikuru3@gmail.com>
2023-04-11 14:02:39 +0000

    added allocation

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * alloc_grid - create two dimentinal array of integers.
+ * @width: width of the array
+ * @height: heigth of the array
+ * Return: size of the 2d array;
+*/
+int **alloc_grid(int width, int height)
+{
+	int **ptr;
+	int i = 0;
+
+	if (width == 0 || height == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = (int **) malloc(height * sizeof(int *));
+
+	for (i = 0; i < height; i++)
+		ptr[i] = (int *) malloc(width * sizeof(int));
+
+	return (ptr);
+}
