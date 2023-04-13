0cb64c7916618f0b2457cd3c73ba743f7731ef8a
Sideeq16 <ajikuru3@gmail.com>
2023-04-13 17:26:11 +0000

    added newly files

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - function that reallocate memory
+ * @ptr: pointer
+ * @old_size: old memory allocated
+ * @new_size: new size of the memory
+ * Return: return new memory allocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr = NULL;
+	unsigned int min_size;
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	if (ptr == NULL)
+	{
+		new_ptr = malloc(new_size);
+	}
+
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == NULL)
+	{
+		return (NULL);
+	}
+	min_size = (old_size < new_size) ? old_size : new_size;
+	memcpy(new_ptr, ptr, min_size);
+	free(ptr);
+
+	return (new_ptr);
+}
