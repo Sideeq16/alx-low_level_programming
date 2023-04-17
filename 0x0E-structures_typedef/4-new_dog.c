8ef98684189c938577609b673e37b5b7092fae85
Sideeq16 <126811305+Sideeq16@users.noreply.github.com>
2023-04-17 22:31:35 +0100

    new_update-4.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,42 +4,42 @@
 #include "dog.h"
 
 /**
- * new_dog - creates a new dog
+ * new_dog - create new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
- *
- * Return: pointer to the new dog or NULL if fails
+ * Return: pointer to the new dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    char *new_name, *new_owner;
+	dog_t *new_dog;
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-    {
-        return (NULL);
-    }
+	new_dog = malloc(sizeof(dog_t));
 
-    new_name = strdup(name);
-    if (new_name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
 
-    new_owner = strdup(owner);
-    if (new_owner == NULL)
-    {
-        free(new_name);
-        free(new_dog);
-        return (NULL);
-    }
+	new_dog->name = strdup(name);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    new_dog->name = new_name;
-    new_dog->age = age;
-    new_dog->owner = new_owner;
+	new_dog->age = age;
 
-    return (new_dog);
+	new_dog->owner = strdup(owner);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
+	/* ptr_dog->name = name ? name : "(nil)"; */
+	/* ptr_dog->age = age; */
+	/* ptr_dog->owner = owner ? owner : "(nil)";*/
+
+	return (new_dog);
 }
