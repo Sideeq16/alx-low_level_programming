8601cc9d8e79b203abdc5aa4fabad604f70c86b5
Sideeq16 <ajikuru3@gmail.com>
2023-04-19 20:49:57 +0000

    if you pend too much

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - print name passed as arg
+ * @array: name pass
+ * @size: function call
+ * @action: callback function
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	long unsigned int i = 0;
+
+	if (action == NULL)
+	{
+		return;
+	}
+
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+
+}
+
