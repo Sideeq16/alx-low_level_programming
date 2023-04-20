8f5834ff66b107cb26f6ef0bd90e3bf1242abd8f
Sideeq16 <ajikuru3@gmail.com>
2023-04-20 03:41:47 +0000

    changes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,18 +2,21 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+	int i;
+	int num_bytes;
+    unsigned char *ptr = (unsigned char *)main;
+
     if (argc != 2) {
         printf("Error\n");
         return 1;
     }
-    int num_bytes = atoi(argv[1]);
+    num_bytes = atoi(argv[1]);
     if (num_bytes < 0) {
         printf("Error\n");
         return 2;
     }
-    unsigned char *ptr = (unsigned char *)main;
-    for (int i = 0; i < num_bytes; i++) {
-        printf("%02x", *(ptr + i));
+    for (i = 0; i < num_bytes; i++) {
+        printf("%x", *(ptr + i));
     }
     printf("\n");
     return 0;
