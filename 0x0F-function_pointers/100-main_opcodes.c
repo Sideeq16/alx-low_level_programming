64bfc756aeabecd94fe8ae69911e82b4d0607926
Sideeq16 <ajikuru3@gmail.com>
2023-04-20 04:05:13 +0000

    Fixed error

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
-void print_opcodes(int num_bytes) {
- int i;
-  unsigned char* ptr = (unsigned char*) &print_opcodes;
-  for (i = 0; i < num_bytes; i++) {
-    printf("%02x ", *ptr++);
-  }
-  printf("\n");
+/**
+ * print_opcodes - opcode
+ * @num_bytes: num of bytes
+ */
+void print_opcodes(int num_bytes)
+{
+	int i;
+	unsigned char *ptr = (unsigned char *) &print_opcodes;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x ", *ptr++);
+	}
+	printf("\n");
 }
 
-int main(int argc, char** argv) {
+/**
+ * main - entry point
+ * @argc: number of argument
+ * @argv: value of argument
+ * Return: 1 or zero
+ */
+int main(int argc, char *argv[])
+{
 	int num_bytes;
-  if (argc != 2) {
-    printf("Error\n");
-    exit(1);
-  }
-  num_bytes = atoi(argv[1]);
-  if (num_bytes < 0)
+
+	if (argc != 2)
+	{
+	printf("Error\n");
+	exit(1);
+	}
+	num_bytes = atoi(argv[1]);
+if (num_bytes < 0)
 {
 	printf("Error\n");
 	exit(2);
 }
 
-  print_opcodes(num_bytes);
-  return 0;
+	print_opcodes(num_bytes);
+	return (0);
 }
