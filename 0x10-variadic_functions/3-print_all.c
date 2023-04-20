84a03ad0f056d4ea2759a6e2773ab62ab1a9526c
Sideeq16 <ajikuru3@gmail.com>
2023-04-20 16:51:43 +0000

    added task 3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include "variadic_functions.h"
+
+/**
+ * print_separator - separate values
+ * @sep: seprator
+ * @condition: conditionn to add comma
+ */
+void print_separator(char *sep, int condition)
+{
+	if (condition && sep != NULL)
+	{
+		printf("%s", sep);
+	}
+}
+
+/**
+ * print_all - print all param passed
+ * @format: string format
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, len;
+
+	len = strlen(format);
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+			case 'c':
+			printf("%c", va_arg(args, int));
+			print_separator(", ", i < (len - 1));
+			break;
+			case 'f':
+			printf("%f", va_arg(args, double));
+			print_separator(", ", i < (len - 1));
+			break;
+			case 'i':
+			printf("%d", va_arg(args, int));
+			print_separator(", ", i < (len - 1));
+			break;
+			case 's':
+			printf("%s", va_arg(args, char *));
+			print_separator(", ", i < (len - 1));
+			break;
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+
+}
--- 0x10-variadic_functions/main.c
+++ 0x10-variadic_functions/main.c
@@ -5,8 +5,10 @@ int main(void)
 {
 	/* int sum;*/
 	/*print_numbers(", ", 4, 0, 98, -1024, 402);*/
-	print_strings(", ", 2, "Jay", "Djago");
-	
+	/* print_strings(", ", 2, "Jay", "Djago"); */
+
+	print_all("ceis", 'B', 3, "stSchool");
+
 	/*printf("%d\n", sum);*/
 	/*sum = sum_them_all(4, 98, 1024, 402, -1024);
 	printf("%d\n", sum); */
