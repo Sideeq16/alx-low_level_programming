5723777c9a06b1d64d2bb8c8c6e54335e681080b
Sideeq16 <ajikuru3@gmail.com>
2023-04-20 03:58:27 +0000

    Fixed error

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 void print_opcodes(int num_bytes) {
+ int i;
   unsigned char* ptr = (unsigned char*) &print_opcodes;
-  for (int i = 0; i < num_bytes; i++) {
+  for (i = 0; i < num_bytes; i++) {
     printf("%02x ", *ptr++);
   }
   printf("\n");
 }
 
 int main(int argc, char** argv) {
-  if (argc < 2) {
-    printf("Usage: ./main number_of_bytes\n");
-    return 1;
+	int num_bytes;
+  if (argc != 2) {
+    printf("Error\n");
+    exit(1);
   }
-  int num_bytes = atoi(argv[1]);
+  num_bytes = atoi(argv[1]);
+  if (num_bytes < 0)
+{
+	printf("Error\n");
+	exit(2);
+}
+
   print_opcodes(num_bytes);
   return 0;
 }
