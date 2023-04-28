318fa9a196dc4ae1c8b8a787c8e6963d0b683573
Sideeq16 <ajikuru3@gmail.com>
2023-04-28 15:58:50 +0000

    insert node at the begining

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - print all node element
+ * @head: head
+ * @str: string to copy
+ * Return: address of the new element
+ */
+
+size_t *add_node(list_t **head, const char *str)
+{
+	list_t *newNode;
+
+	newNode = malloc(sizeof(list_t));
+
+	if (newNode == NULL)
+	{
+		return (NULL);
+	}
+
+	newNode->str = strup(str);
+	newNode->len = strlen(str);
+	newNode->next = *head;
+	*head = newNode;
+
+	return (newNode);
+}
