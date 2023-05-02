e5903a711aef9adb0160ba7656f78c008daf0c5e
Sideeq16 <ajikuru3@gmail.com>
2023-05-02 20:31:38 +0000

    add task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum_listint - sum data of a node
+ * @head: node head
+ * Return: sum of the data
+ */
+int sum_listint(listint_t **head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
