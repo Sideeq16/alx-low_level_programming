93b1d77bd6ac9550ddb389e7ac86f124defb034d
Sideeq16 <ajikuru3@gmail.com>
2023-05-04 17:47:53 +0000

    task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_bit - number integer to binary
+ * @n: number
+ * @index: index to locate
+ * Return: locate the index of a binary of number
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	return ((n >> index) & 1);
+
+}
