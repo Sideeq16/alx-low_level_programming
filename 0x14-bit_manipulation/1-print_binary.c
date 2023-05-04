0ca3a7f110feefd09a5d1cd592ad717fe93206dc
Sideeq16 <ajikuru3@gmail.com>
2023-05-04 17:10:32 +0000

    added task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_binary - number integer to binary
+ * @n: number to convert
+ * Return: binary representation of number
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+
+}
