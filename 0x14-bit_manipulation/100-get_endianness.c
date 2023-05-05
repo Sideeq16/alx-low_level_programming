14b7c8ee3c372b89236fd08843084c3724a3a3d2
Sideeq16 <ajikuru3@gmail.com>
2023-05-05 14:46:27 +0000

    added 100 task

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * get_endianness - get endianness of the system
+ * Return: 1 0R 0
+ */
+
+int get_endianness(void)
+{
+	unsingned int i = 1;
+	char *c = (char *) &i;
+
+	return ((int)*c);
+}
