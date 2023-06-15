364025939ce168ce479658a5f3a1ee2af52eb1d2
Sideeq16 <ajikuru3@gmail.com>
2023-06-15 16:53:25 +0000

    added task 3

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - print element in doubly link
+ * @h: head node
+ * @n: node data
+ * Return: number of elmement in node
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *temp;
+
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
+	temp->n = n;
+	temp->prev = NULL;
+	temp->next = *head;
+	if (*head != NULL)
+		(*head)->prev = temp;
+	*head = temp;
+	return (temp);
+}
